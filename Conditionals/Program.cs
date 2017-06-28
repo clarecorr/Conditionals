627c2ff0d8181c2d3332e16bbd603d7f7421e6c8
clarecorr <clare.e.corrigan@gmail.com>
2017-06-28 13:45:02 -0400

    class exercises

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -10,6 +10,35 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("How many people are in your party?");
+            int numberOfPeople = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is your total bill?");
+            double totalBill = double.Parse(Console.ReadLine());
+
+            //Define billWithDiscount outside of conditional statements
+            double billWithDiscount;
+           
+
+            if (totalBill >= 50)
+            {
+                billWithDiscount = totalBill * .9;
+            }
+            else
+            {
+                billWithDiscount = totalBill * .95;
+            }
+
+            double costPerPerson = billWithDiscount / numberOfPeople;
+            Console.WriteLine("Your individual total is: $" + costPerPerson);
+
+
+
+
+
+
         }
     }
 }
+
